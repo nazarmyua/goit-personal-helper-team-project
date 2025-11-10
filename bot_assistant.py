94d9433387e6289bb8549bb794251b7aab2f2970
Mike <45781337+mikeKuharuk@users.noreply.github.com>
2025-11-10 19:28:17 +0100

    Summary of all home works

---
+++ bot_assistant.py
@@ -0,0 +1,228 @@
+# Command constants
+from address_book import AddressBook, Record
+import pickle
+
+QUIT_COMMANDS = ["close", "exit", "quit", "q"]
+HELP_COMMANDS = ["help", "h"]
+HELLO_COMMANDS = ["hello", "hi", "hey"]
+ADD_CONTACT = ["add"]
+REMOVE_CONTACT = ["remove"]
+UPDATE_CONTACT = ["change"]
+FIND_CONTACT = ["phone"]
+ALL_CONTACTS = ["all"]
+ADD_BIRTHDAY = ["add-birthday"]
+SHOW_BIRTHDAY = ["show-birthday"]
+GET_UPCOMING_BIRTHDAYS = ["birthdays"]
+
+ADDRESS_BOOK_CACHE_LOCATION = "address_book.pkl"
+
+
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            if func.__name__ == "add_contact":
+                return "Please provide Name and phone number"
+            if func.__name__ == "add_birthday":
+                return "Please provide Name and date in format dd.mm.yyyy"
+            if func.__name__ == "change_contact":
+                return "Please provide Name, old number and new number"
+            if func.__name__ in ("show_birthday",
+                                 "find_contact", "remove_contact"):
+                return "Please provide Name"
+            else:
+                raise ValueError
+        except AttributeError as e:
+            if func.__name__ in "show_birthday":
+                if "name" in e.args[0]:
+                    return "This contact does not exist"
+                elif "birthday" in e.args[0]:
+                    return "This contact does not have a birthday record"
+
+            if func.__name__ in ["show_birthday", "find_contact",
+                                 "remove_contact", "change_contact"]:
+                return "This contact does not exist"
+
+        except KeyboardInterrupt:
+            quit()
+
+    return inner
+
+
+@input_error
+def parse_input(raw_input: str) -> tuple[str, list[str]]:
+    if raw_input == "":
+        return "", []
+
+    cmd, *args = raw_input.split()
+    cmd = cmd.strip().lower()
+
+    return cmd, *args
+
+
+@input_error
+def add_contact(args, book: AddressBook):
+    name, phone, *_ = args
+    record = book.find(name)
+    message = "Contact updated."
+    if record is None:
+        record = Record(name)
+        book.add_record(record)
+        message = "Contact added."
+    if phone:
+        record.add_phone(phone)
+    return message
+
+
+@input_error
+def remove_contact(args, book: AddressBook) -> str:
+    name, *_ = args
+    book.delete(name)
+    return "Contact removed: {}".format(name)
+
+
+@input_error
+def change_contact(args, book: AddressBook) -> str:
+    name, old_phone, new_phone, *_ = args
+
+    record = book.find(name)
+    record.edit_phone(old_phone, new_phone)
+    return "Phone changed."
+
+
+@input_error
+def find_contact(args, book: AddressBook) -> Record | None:
+    name, *_ = args
+    record = book.find(name)
+    return record
+
+
+@input_error
+def find_all_contacts(book: AddressBook) -> str:
+    if len(book) == 0:
+        return "Contacts book is empty"
+
+    all_contacts_str = ""
+    for name, record in book.items():
+        all_contacts_str += f"{record}\n"
+    return all_contacts_str
+
+
+@input_error
+def add_birthday(args, book: AddressBook) -> str:
+    name, birthday, *_ = args
+    record = book.find(name)
+    record.add_birthday(birthday)
+    return "Birthday added."
+
+
+@input_error
+def show_birthday(args, book: AddressBook) -> str:
+    name, *_ = args
+    record = book.find(name)
+    return f"{record.name} - {record.birthday}"
+
+
+@input_error
+def get_upcoming_birthdays(book: AddressBook) -> str:
+    upcoming_birthdays = book.get_upcoming_birthdays()
+    to_congratulate = "Next 7 days birthdays:"
+    for b_day in upcoming_birthdays:
+        to_congratulate += f"\n{b_day['name']} {b_day['congratulation_date']}"
+
+    return to_congratulate
+
+
+@input_error
+def handle_input(address_book, command, *args):
+    if command in QUIT_COMMANDS:
+        save_data(address_book)
+        print("Good bye!")
+        raise KeyboardInterrupt
+
+    elif command in HELLO_COMMANDS:
+        print("How can I help you?")
+
+    elif command in HELP_COMMANDS:
+        print("Here are the list of commands:"
+              f"\nQuit commands: {QUIT_COMMANDS}"
+              f"\nHelp commands: {HELP_COMMANDS}"
+              f"\nHello commands: {HELLO_COMMANDS}"
+              f"\nAdd contact: {ADD_CONTACT} Name Phone. "
+              f"\nRemove contact: {REMOVE_CONTACT} Name"
+              f"\nUpdate contact: {UPDATE_CONTACT} Name Phone. "
+              f"\nFind contact: {FIND_CONTACT} Name. "
+              f"\nShow all contacts: {ALL_CONTACTS}"
+              f"\nAdd birthday: {ADD_BIRTHDAY} Name dd.mm.yyyy "
+              f"\nShow birthday: {SHOW_BIRTHDAY} Name"
+              f"\nGet upcoming: {GET_UPCOMING_BIRTHDAYS}")
+
+    elif command in ADD_CONTACT:
+        print(add_contact(args, address_book))
+
+    elif command in REMOVE_CONTACT:
+        print(remove_contact(args, address_book))
+
+    elif command in UPDATE_CONTACT:
+        print(change_contact(args, address_book))
+
+    elif command in FIND_CONTACT:
+        print(find_contact(args, address_book))
+
+    elif command in ALL_CONTACTS:
+        print(find_all_contacts(address_book))
+
+    elif command in ADD_BIRTHDAY:
+        print(add_birthday(args, address_book))
+
+    elif command in SHOW_BIRTHDAY:
+        print(show_birthday(args, address_book))
+
+    elif command in GET_UPCOMING_BIRTHDAYS:
+        print(get_upcoming_birthdays(address_book))
+
+    else:
+        print("Invalid command. Try again, type 'help' for commands tips")
+
+    # Writing to cache file after each operation to ensure that all data is saved
+    save_data(address_book)
+
+def init_address_book() -> AddressBook:
+    try:
+        with open(ADDRESS_BOOK_CACHE_LOCATION, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError or pickle.UnpicklingError:
+        return AddressBook()
+
+def save_data(book):
+    with open(ADDRESS_BOOK_CACHE_LOCATION, "wb") as f:
+        pickle.dump(book, f)
+
+def load_data():
+    try:
+        with open(ADDRESS_BOOK_CACHE_LOCATION, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return AddressBook()  # Повернення нової адресної книги, якщо файл не знайдено
+
+
+def main():
+    address_book = init_address_book()
+
+    print("Greetings! I'm Jarvis, your personal assistant."
+          "\nHow can I help you today?"
+          "\nType 'help' for more information.")
+
+    while True:
+        user_input = input(">>>")
+        command, *args = parse_input(user_input)
+        handle_input(address_book, command, *args)
+
+def run():
+    # Proxy function to boot up Bot from outer module
+    main()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests.py
@@ -0,0 +1,101 @@
+import bot_assistant
+from address_book import AddressBook
+
+
+def main():
+    run_tests(AddressBook())
+
+
+def execute(str_input, address_book):
+    command, *args = bot_assistant.parse_input(str_input)
+    bot_assistant.handle_input(address_book, command, *args)
+
+
+def run_tests(address_book: AddressBook) -> None:
+    # Add Mike
+    execute("add Mike 1234567890", address_book)
+
+    # Add him a birthday
+    execute("add-birthday Mike 05.11.1997", address_book)
+
+    # Print his birthday
+    execute("show-birthday Mike", address_book)
+
+    # Add Kate
+    execute("add Kate 1111111111", address_book)
+
+    # Add her extra number
+    execute("add Kate 1111111112", address_book)
+
+    # Add her b-day
+    execute("add-birthday Kate 04.11.1991", address_book)
+
+    # Add Paolinka without birthday
+    execute("add Paolinka 1111111113", address_book)
+
+    # Print all records
+    execute("all", address_book)
+
+    # Change Mikes number and print updated number
+    execute("change Mike 1234567890 0987654321", address_book)
+    execute("phone Mike", address_book)
+    # Get all upcoming b-days
+    execute("birthdays", address_book)
+
+    # Add and remove Jovani, check that he is removed
+    execute("add Jovani 1111111112", address_book)
+    execute("remove Jovani", address_book)
+    execute("all", address_book)
+
+    # Input mistakes
+    execute("add Jovani 111111111", address_book)
+
+    execute("add Jovani OneOneOne", address_book)
+
+    execute("remove Jovani", address_book)
+    execute("remove Jovani please 123", address_book)
+
+    execute("phone Jovani", address_book)
+
+    execute("show-birthday Jovani", address_book)
+
+    execute("show-birthday", address_book)
+    execute("show-birthday ", address_book)
+    execute("phone", address_book)
+    execute("phone ", address_book)
+    execute("add", address_book)
+    execute("add ", address_book)
+    execute("remove", address_book)
+    execute("remove ", address_book)
+    execute("add-birthday ", address_book)
+
+    execute("add-birthday Jovani", address_book)
+    execute("add-birthday Mike 20-11-1997", address_book)
+    execute("add-birthday Mike 20-11", address_book)
+    execute("add-birthday Mike ", address_book)
+
+    execute("change", address_book)
+    execute("change ", address_book)
+    execute("change Jovani ", address_book)
+
+    execute("remove 1", address_book)
+    execute("change 2", address_book)
+
+    execute("a", address_book)
+    execute("", address_book)
+
+    execute("add Stephan 1231233330", address_book)
+    execute("show-birthday Stephan", address_book)
+    execute("birthdays", address_book)
+
+    print("\n")
+
+    execute("show-birthday Nicolaos", address_book)
+    execute("show-birthday Nicolaos 20.11.1997", address_book)
+    execute("change Nicolaos 1231231231 3213213213", address_book)
+    execute("phone Nicolaos", address_book)
+    execute("remove Nicolaos", address_book)
+
+
+if __name__ == "__main__":
+    main()
